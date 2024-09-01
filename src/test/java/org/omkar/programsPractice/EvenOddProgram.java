60d6697ec95d9e72574fae2de6f2daf2d11f925c
ompalande123 <ompalande123@gmail.com>
2024-09-01 23:43:47 +0530

    Even odd number program with scanner class...

--- src/test/java/org/omkar/programsPractice/EvenOddProgram.java
+++ src/test/java/org/omkar/programsPractice/EvenOddProgram.java
@@ -1,10 +1,31 @@
 package org.omkar.programsPractice;
 
+import java.util.Scanner;
+
 public class EvenOddProgram {
 
 	public static void main(String[] args) {
 		
-		int n=10;
+		
+		
+		
+		
+		
+		// Using scanner class
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter an integer value: ");
+		int number=sc.nextInt();
+		
+		if(number%2==0)
+		{
+			System.out.println(number+ " is even number.");
+			
+		}
+		else
+		{
+			System.out.println(number+ " is odd number.");
+			
+		}
 		
 
 	}
